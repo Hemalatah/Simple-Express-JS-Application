8b952382de43e0cc6222bf9c5de822bc0dfd5971
Hemalatha <11727944+Hemalatah@users.noreply.github.com>
2018-08-09 02:04:17 -0700

    Add server code

---
+++ server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+
+router.get("/",function(req,res){
+	res.sendFile(__dirname + "/public/index.html");
+});
+
+router.get("/user",function(req,res){
+	res.sendFile(__dirname + "/public/user.html");
+});
+
+var requests = {};
+requests['1'] = 10;
+router.get("/user/:id",function(req,res){
+	var user = req.params.id;
+	if(!requests[user]) {
+		requests[user] = 1;
+	} 
+	else {
+  		requests[user] += 1;
+  	}
+  	res.send({"No. of Requests" : "Hello User-"+user+", You have made "+requests[user]+" requests"});
+});
+
+router.get("/math",function(req,res){
+  res.sendFile(__dirname + "/public/math.html");
+});
+
+router.get("/game",function(req,res){
+  res.sendFile(__dirname + "/public/game.html");
+});
+
+app.use("/",router);
+
+app.use("/*",function(req,res){
+  res.sendFile(__dirname + "/public/404.html");
+});
+
+var server = app.listen(5000,function(){
+  console.log("Live at Port 5000");
+});
+
+module.exports = server;
